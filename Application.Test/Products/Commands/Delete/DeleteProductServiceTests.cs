b09e330b22496abbe0519a1d7945bdb8bef5bfd1
arashazimi0032 <arashazimi0032@gmail.com>
2023-12-29 01:51:46 +0330

    write test for Update and Delete Product Services

---
+++ Application.Test/Products/Commands/Delete/DeleteProductServiceTests.cs
@@ -0,0 +1,98 @@
+﻿using Application.Products.Commands.Delete;
+using Domain.Exceptions;
+using Domain.IRepositories.Commands;
+using Domain.IRepositories.UnitOfWorks;
+using Domain.Products;
+using Domain.Products.ValueObjects;
+using Domain.Shared.ValueObjects;
+
+namespace Application.Test.Products.Commands.Delete;
+
+public class DeleteProductServiceTests
+{
+    private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+    private readonly Mock<ICommandUnitOfWork> _commandUnitOfWorkMock;
+    private readonly Mock<IProductCommandRepository> _productCommandRepositoryMock;
+
+    public DeleteProductServiceTests()
+    {
+        _unitOfWorkMock = new();
+        _commandUnitOfWorkMock = new();
+        _productCommandRepositoryMock = new();
+    }
+
+    [Fact]
+    public async Task Service_Should_ThrowProductNotFoundException_WhenProductIsNull()
+    {
+        // Arrange
+        var deleteProductService = new DeleteProductService(_unitOfWorkMock.Object);
+        var productId = ProductId.CreateUnique();
+        var cancellationToken = It.IsAny<CancellationToken>();
+
+        _unitOfWorkMock.Setup(
+                x => x.Queries.Product.GetByIdAsync(
+                    productId,
+                    cancellationToken))
+            .ReturnsAsync((Product?)null);
+
+        //Act
+        var act = async () => await deleteProductService.DeleteProduct(productId.Value, cancellationToken);
+
+
+        //Assert
+        await act.Should().ThrowAsync<ProductNotFoundException>();
+    }
+
+    [Fact]
+    public async Task Service_Should_CallRemoveOnUnitOfWorkCommandProduct_WhenProductIsNotNull()
+    {
+        // Arrange
+        var deleteProductService = new DeleteProductService(_unitOfWorkMock.Object);
+        var productId = ProductId.CreateUnique();
+        var cancellationToken = It.IsAny<CancellationToken>();
+        var price = new Money("USD", 100m);
+        var product = Product.Create("Product", price);
+
+        _unitOfWorkMock.Setup(
+                x => x.Queries.Product.GetByIdAsync(
+                    productId,
+                    cancellationToken))
+            .ReturnsAsync(product);
+
+        _unitOfWorkMock.SetupGet(x => x.Commands).Returns(_commandUnitOfWorkMock.Object);
+        _unitOfWorkMock.SetupGet(x => x.Commands.Product).Returns(_productCommandRepositoryMock.Object);
+
+        //Act
+        await deleteProductService.DeleteProduct(productId.Value, cancellationToken);
+
+
+        //Assert
+        _unitOfWorkMock.Verify(
+            x => x.Commands.Product.Remove(product),
+            Times.Once());
+        _unitOfWorkMock.Verify(x => x.SaveChangesAsync(cancellationToken),
+            Times.Once);
+    }
+
+    [Fact]
+    public async Task Service_Should_NotCallSaveChanges_WhenProductIsNull()
+    {
+        // Arrange
+        var deleteProductService = new DeleteProductService(_unitOfWorkMock.Object);
+        var productId = ProductId.CreateUnique();
+        var cancellationToken = It.IsAny<CancellationToken>();
+
+        _unitOfWorkMock.Setup(
+                x => x.Queries.Product.GetByIdAsync(
+                    productId,
+                    cancellationToken))
+            .ReturnsAsync((Product?)null);
+
+        //Act
+        var act = async () => await deleteProductService.DeleteProduct(productId.Value, cancellationToken);
+
+
+        //Assert
+        _unitOfWorkMock.Verify(x => x.SaveChangesAsync(cancellationToken), Times.Never);
+    }
+}
---
+++ Application.Test/Products/Commands/Update/UpdateProductServiceTests.cs
@@ -0,0 +1,120 @@
+﻿using Application.Products.Commands.Update;
+using Domain.Exceptions;
+using Domain.IRepositories.Commands;
+using Domain.IRepositories.UnitOfWorks;
+using Domain.Products;
+using Domain.Products.ValueObjects;
+using Domain.Shared.ValueObjects;
+using Microsoft.CodeAnalysis;
+using Microsoft.Extensions.Caching.Memory;
+using System.Threading;
+
+namespace Application.Test.Products.Commands.Update;
+
+public class UpdateProductServiceTests
+{
+    private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+    private readonly Mock<IMemoryCache> _memoryCacheMock;
+    private readonly Mock<ICommandUnitOfWork> _commandUnitOfWorkMock;
+    private readonly Mock<IProductCommandRepository> _productCommandRepositoryMock;
+
+    public UpdateProductServiceTests()
+    {
+        _unitOfWorkMock = new ();
+        _memoryCacheMock = new();
+        _commandUnitOfWorkMock = new();
+        _productCommandRepositoryMock = new ();
+    }
+
+    [Fact]
+    public async Task Service_Should_ThrowProductNotFoundException_WhenProductIsNull()
+    {
+        // Arrange
+        var updateProductService = new UpdateProductService(_unitOfWorkMock.Object, _memoryCacheMock.Object);
+        var productId = ProductId.CreateUnique();
+        var price = new Money("USD", 100m);
+        var cancellationToken = It.IsAny<CancellationToken>();
+
+        _unitOfWorkMock.Setup(
+                x => x.Queries.Product.GetByIdAsync(
+                    productId,
+                    cancellationToken))
+            .ReturnsAsync((Product?)null);
+
+        // Act
+        var act = async () => await updateProductService.UpdateProduct(
+            productId.Value,
+            "testName", 
+            price, 
+            cancellationToken);
+
+        // Assert
+        await act.Should().ThrowAsync<ProductNotFoundException>();
+    }
+
+    [Fact]
+    public async Task Service_Should_CallAddOnUnitOfWork_WhenProductIsNotNull()
+    {
+        // Arrange
+        var guidValue = Guid.NewGuid();
+
+        var updateProductService = new UpdateProductService(_unitOfWorkMock.Object, _memoryCacheMock.Object);
+
+        var productId = ProductId.Create(guidValue);
+        var price = new Money("USD", 100m);
+        var product = Product.Create("New Product", price);
+        var cancellationToken = It.IsAny<CancellationToken>();
+
+        _unitOfWorkMock.Setup(
+                x => x.Queries.Product.GetByIdAsync(
+                    productId,
+                    cancellationToken))
+            .ReturnsAsync(product);
+
+        _unitOfWorkMock.SetupGet(x => x.Commands).Returns(_commandUnitOfWorkMock.Object);
+        _unitOfWorkMock.SetupGet(x => x.Commands.Product).Returns(_productCommandRepositoryMock.Object);
+
+        // Act
+        await updateProductService.UpdateProduct(
+            productId.Value,
+            "product Name Updated",
+            price, 
+            cancellationToken);
+
+        // Assert
+        _unitOfWorkMock.Verify(
+            x => x.Commands.Product.Update(product),
+            Times.Once);
+        _unitOfWorkMock.Verify(
+            x => x.SaveChangesAsync(It.IsAny<CancellationToken>()), 
+            Times.Once);
+    }
+
+    [Fact]
+    public async Task Service_ShouldNotCallSaveChanges_WhenProductIsNull()
+    {
+        // Arrange
+        var updateProductService = new UpdateProductService(_unitOfWorkMock.Object, _memoryCacheMock.Object);
+        var productId = ProductId.CreateUnique();
+        var price = new Money("USD", 100m);
+        var cancellationToken = It.IsAny<CancellationToken>();
+
+        _unitOfWorkMock.Setup(
+                x => x.Queries.Product.GetByIdAsync(
+                    productId,
+                    cancellationToken))
+            .ReturnsAsync((Product?)null);
+
+        // Act
+        var act = async () => await updateProductService.UpdateProduct(
+            productId.Value,
+            "testName",
+            price,
+            cancellationToken);
+
+        // Assert
+        _unitOfWorkMock.Verify(
+            x => x.SaveChangesAsync(cancellationToken),
+            Times.Never);
+    }
+}
