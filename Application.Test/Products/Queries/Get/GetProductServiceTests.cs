b0d2b3917c0ad6755670703b19c32cb99a14ace1
arashazimi0032 <arashazimi0032@gmail.com>
2023-12-29 22:26:57 +0330

    complete product Tests in Application Layer with Get and GetAll services Tests.

---
+++ Application.Test/Products/Queries/Get/GetProductServiceTests.cs
@@ -0,0 +1,64 @@
+﻿using Application.Products.Queries.Get;
+using Application.Products.Queries.Share;
+using Domain.Exceptions;
+using Domain.IRepositories.UnitOfWorks;
+using Domain.Products;
+using Domain.Products.ValueObjects;
+using Domain.Shared.ValueObjects;
+
+namespace Application.Test.Products.Queries.Get;
+
+public class GetProductServiceTests
+{
+    private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+
+    public GetProductServiceTests()
+    {
+        _unitOfWorkMock = new();
+    }
+
+    [Fact]
+    public async Task Service_Should_ThrowProductNotFoundException_WhenProductIsNull()
+    {
+        // Arrange
+        var getProductService = new GetProductService(_unitOfWorkMock.Object);
+        var productId = ProductId.CreateUnique();
+        var cancellationToken = It.IsAny<CancellationToken>();
+
+        _unitOfWorkMock.Setup(x => x.Queries.Product.GetByIdFromMemoryCacheAsync(
+                productId,
+                cancellationToken))
+            .ReturnsAsync((Product?)null);
+
+        // Act
+        var act = async () => await getProductService.GetProduct(productId.Value, cancellationToken);
+
+        // Assert
+        await act.Should().ThrowAsync<ProductNotFoundException>();
+    }
+
+    [Fact]
+    public async Task Service_Should_ReturnProductResponse_WhenProductIsNotNull()
+    {
+        // Arrange
+        var getProductService = new GetProductService(_unitOfWorkMock.Object);
+        var productId = ProductId.CreateUnique();
+        var cancellationToken = It.IsAny<CancellationToken>();
+        var productName = "Product Name";
+        var price = new Money("USD", 100m);
+        var product = Product.Create(productName, price);
+        var productResponse = new ProductResponse(productId.Value, productName, price);
+
+        _unitOfWorkMock.Setup(x => x.Queries.Product.GetByIdFromMemoryCacheAsync(
+                productId,
+                cancellationToken))
+            .ReturnsAsync(product);
+
+        // Act
+        var act = await getProductService.GetProduct(productId.Value, cancellationToken);
+
+        // Assert
+        act.Should().NotBeNull();
+        act.Should().Be(productResponse);
+    }
+}
---
+++ Application.Test/Products/Queries/GetAll/GetAllProductServiceTests.cs
@@ -0,0 +1,46 @@
+﻿using Application.Products.Queries.GetAll;
+using Application.Products.Queries.Share;
+using Domain.IRepositories.UnitOfWorks;
+using Domain.Products;
+using Domain.Shared.ValueObjects;
+
+namespace Application.Test.Products.Queries.GetAll;
+
+public class GetAllProductServiceTests
+{
+    private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+
+    public GetAllProductServiceTests()
+    {
+        _unitOfWorkMock = new();
+    }
+
+    [Fact]
+    public async Task Service_Should_ReturnListOfProductResponses_WhenSuccess()
+    {
+        // Arrange
+        var getAllProductService = new GetAllProductsService(_unitOfWorkMock.Object);
+        var cancellationToken = It.IsAny<CancellationToken>();
+
+        List<Product> products = new()
+        {
+            Product.Create("Product 1", new Money("USD", 100m)),
+            Product.Create("Product 2", new Money("USD", 150m))
+        };
+
+        var productResponse = products
+            .Select(p => new ProductResponse(p.Id.Value, p.Name, p.Price))
+            .ToList();
+
+        _unitOfWorkMock
+            .Setup(x => x.Queries.Product.GetAllAsNoTrackAsync(cancellationToken))
+            .ReturnsAsync(products);
+
+        // Act
+        var response = await getAllProductService.GetAllProducts(cancellationToken);
+
+        // Assert
+        response.Should().BeEquivalentTo(productResponse);
+
+    }
+}
